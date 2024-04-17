ecf499bbc201c5bd2158dc2ce0f4fc2039fdae84
IvayloAnaniev <Zorc213@gmail.com>
2024-04-17 22:24:13 +0300

    task3

--- Tasks/task3.js
+++ Tasks/task3.js
@@ -1,6 +1,7 @@
 function solve(num1, num2, num3) {
     let sum = num1 + num2 + num3;
     let output = sum % 1 === 0 ? `$(sum) - Integer` : `$(sum) - float`;
+    console.log(output);
 }
 
-solve(9, 100, 1.1);
+solve(9, 100, 10);
